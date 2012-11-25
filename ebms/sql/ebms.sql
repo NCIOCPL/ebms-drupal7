94b45f5c2147d70f05b7c1490042ef54489a5886
alan <webcommauto@users.noreply.github.com>
2012-11-25 21:29:20 +0000

    <R10831> Added an index on ebms_article.import_date.

--- ebms/sql/ebms.sql
+++ ebms/sql/ebms.sql
@@ -417,6 +417,8 @@ CREATE TABLE ebms_article (
              ON ebms_article(jrnl_title);
       CREATE INDEX ebms_article_brf_jrnl_title_index
              ON ebms_article(brf_jrnl_title);
+      CREATE INDEX ebms_article_import_date
+             ON ebms_article(import_date);
       CREATE INDEX ebms_article_published_date
              ON ebms_article(published_date);
 
