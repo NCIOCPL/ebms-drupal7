aa45b1cae03ab77f8f3e5ffff42175b13d4d7ce2
Bob Kline <bkline@rksystems.com>
2013-02-06 20:11:12 +0000

    <R11352> Tooltip help for PMID field

---
+++ ebms/ebms.nci.nih.gov/modules/custom/ebms/js/import.js
@@ -0,0 +1,5 @@
+jQuery(function() {
+    var help = "Enter space- or comma-delimited Pubmed IDs or choose a " +
+        "file containing a Pubmed Print format search results file below.";
+    jQuery("#import-form div.form-item-pmid").attr("title", help);
+});
