b67793404bd7699cbeb9af01705d1dbbcdacb8b8
Bob Kline <bkline@rksystems.com>
2013-02-06 19:19:49 +0000

    <R11350> Added testers' theme to version control

---
+++ ebms/ebms.nci.nih.gov/themes/apitest/html.tpl.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php print $head_title; ?></title>
+      <?php print $styles; ?>
+  </head>
+  <body>
+    <?php /* print $page_top; */ ?>
+<?php print $page; ?>
+    <?php /* print $page_bottom; */ ?>
+  </body>
+</html>
---
+++ ebms/ebms.nci.nih.gov/themes/apitest/page.tpl.php
@@ -0,0 +1,3 @@
+    <div id="message-wrapper"><?php print $messages; ?></div>
+    <h2 class="title" id="page-title"><?php print $title; ?></h2>
+<?php print render($page['content']); ?>
