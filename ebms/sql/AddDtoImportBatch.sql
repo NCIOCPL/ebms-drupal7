a8edb03888fadf16c0e6c6ecbff98591ea5f8f8f
alan <webcommauto@users.noreply.github.com>
2014-01-03 14:33:35 +0000

    <R12233> Added two new columns for more error handling.  (JIRA::OCEEBMS-119)

--- ebms/sql/AddDtoImportBatch.sql
+++ ebms/sql/AddDtoImportBatch.sql
@@ -1,3 +1,10 @@
-USE ebms;
+USE oce_ebms;
+
 ALTER TABLE ebms_import_batch
 MODIFY input_type ENUM('R','F','S','D');
+
+ALTER TABLE ebms_import_batch
+ADD COLUMN messages TEXT NULL;
+
+ALTER TABLE ebms_import_batch
+ADD COLUMN status VARCHAR(32) NOT NULL DEFAULT 'Success';
