963c8073874e2c5d9d282445a72e6449ba05c400
Bob Kline <bkline@rksystems.com>
2019-08-03 10:23:14 -0400

    Add high_priority article tag (OCEEBMS-469)

---
+++ sql/oceebms-469.sql
@@ -0,0 +1,4 @@
+INSERT ebms_article_tag_type (text_id, tag_name, description, topic_required)
+VALUES ('high_priority', 'High priority',
+        'The article needs expedited review by the board (for example, because it '
+        'has been provided by the EIC or another channel or in the press).', 'Y');
