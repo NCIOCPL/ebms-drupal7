3b5ced9c9a499356afbba934c881956a8bcfd434
Bob Kline <bkline@rksystems.com>
2020-08-26 15:52:26 -0400

    Shorten text_id (OCEEBMS-567)

--- sql/oceebms-567.sql
+++ sql/oceebms-567.sql
@@ -1,3 +1,3 @@
 INSERT ebms_article_tag_type (text_id, tag_name, description, topic_required)
-VALUES ('comment_for_librarian', 'Comment for Librarian',
+VALUES ('librarian-cmt', 'Comment for Librarian',
         'Feedback for the medical librarians.', 'Y');
