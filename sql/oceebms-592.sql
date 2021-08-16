cc314778abf9243e9cfa87bc3e30fa77cea3f798
Bob Kline <bkline@rksystems.com>
2021-08-16 09:31:50 -0400

    Rename tag (changed requirements for OCEEBMS-592)

--- sql/oceebms-592.sql
+++ sql/oceebms-592.sql
@@ -1,2 +1,2 @@
 INSERT ebms_article_tag_type (text_id, tag_name, description, topic_required)
-VALUES ('preliminary', 'Preliminary', 'Article in a non-final state', 'N');
+VALUES ('preliminary', 'Preliminary PDF', 'Article in a non-final state', 'N');
